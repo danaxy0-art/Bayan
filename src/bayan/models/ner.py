cb48dc74ebca4852ca09f8873e356319c2e3a7f1
Dana <danaxy0@gmail.com>
2026-09-09 17:50:40 +0300

    feat(ner): implement align_labels for BIO subword alignment

--- src/bayan/models/ner.py
+++ src/bayan/models/ner.py
@@ -2,5 +2,22 @@
 
 
 def align_labels(word_ids, word_labels):
-    # TODO(Lab 3B): align BIO labels to subwords and mask non-first pieces with -100.
-    raise NotImplementedError
+    """Align word-level BIO labels to subword tokens."""
+
+    aligned_labels = []
+    previous_word_id = None
+
+    for word_id in word_ids:
+
+        if word_id is None:
+            aligned_labels.append(-100)
+
+        elif word_id != previous_word_id:
+            aligned_labels.append(word_labels[word_id])
+
+        else:
+            aligned_labels.append(-100)
+
+        previous_word_id = word_id
+
+    return aligned_labels
